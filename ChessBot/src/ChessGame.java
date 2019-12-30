4ec7fee83490c844a54f1c119ec23465f59336a5
jchua227 <jchua@albany.edu>
2019-12-29 19:08:12 -0500

    added chess images pieces

--- ChessBot/src/ChessGame.java
+++ ChessBot/src/ChessGame.java
@@ -1,16 +1,24 @@
+import Pieces.Knight;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 import java.awt.*;
 import java.util.List;
 
-public class ChessGame {
+public class ChessGame extends JFrame{
     private JPanel jPanel;
     private JButton newGameButton;
 
+
     public ChessGame(){
+
         newGameButton.addActionListener(new ActionListener(){
+
             @Override
             public void actionPerformed(ActionEvent e){
                 clearBoard();
@@ -20,9 +28,8 @@ public class ChessGame {
 
     public static void main(String []args){
         JFrame jFrame = new JFrame("Chess Game: Made by Jared Chua");
-        jFrame.setPreferredSize(new Dimension(2200,1500));
-        jFrame.setLocation(600,200);
-        jFrame.setResizable(false);
+        jFrame.setPreferredSize(new Dimension(1200,1000));
+        jFrame.setLocation(400,25);
         jFrame.setContentPane(new ChessGame().jPanel);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.pack();
@@ -31,6 +38,7 @@ public class ChessGame {
     }
 
     public void clearBoard(){
-
+        
     }
+
 }
