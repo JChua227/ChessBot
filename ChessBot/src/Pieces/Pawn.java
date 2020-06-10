feffaf492a33ec26dcd2754db9a91e9b95ddbf2d
jchua227 <jchua@albany.edu>
2020-06-10 10:47:30 -0400

    pawn can now promote

--- ChessBot/src/Pieces/Pawn.java
+++ ChessBot/src/Pieces/Pawn.java
@@ -23,17 +23,22 @@ public class Pawn extends Piece{
 
         List<Move> list = new ArrayList<>();
 
-        if(x+forward>-1 && x+forward<gameBoard.length && gameBoard[x+forward][y]==null){
-            Piece[][] possiblePosition = this.copy(gameBoard);
-            Pawn pawn = new Pawn(gameBoard[x][y].getPlayerPiece());
-            pawn.setMoved(true);
-            possiblePosition[x][y] = null;
-            possiblePosition[x+forward][y] = pawn;
-            List<String> newNotation = this.generateNewNotation(notation, x, y, x+forward, y);
-            Move move = new Move(x, y, x+forward, y, possiblePosition, newNotation);
-            list.add(move);
+        if(gameBoard[x+forward][y]==null){
+            if(x+forward==gameBoard.length-1 || x+forward==0){
+                pawnPromotion(gameBoard,x,y,notation,list,forward,0);
+            }
+            else {
+                Piece[][] possiblePosition = this.copy(gameBoard);
+                Pawn pawn = new Pawn(gameBoard[x][y].getPlayerPiece());
+                pawn.setMoved(true);
+                possiblePosition[x][y] = null;
+                possiblePosition[x + forward][y] = pawn;
+                List<String> newNotation = this.generateNewNotation(notation, x, y, x + forward, y);
+                Move move = new Move(x, y, x + forward, y, possiblePosition, newNotation);
+                list.add(move);
+            }
         }
-        System.out.println(gameBoard[x][y].getPlayerPiece() + " " + x + " " + y + " " + forward + " " + gameBoard[x][y].getMoved());
+
         if(!gameBoard[x][y].getMoved() && gameBoard[x+forward][y]==null && gameBoard[x+forward+forward][y]==null){
             Piece[][] possiblePosition = this.copy(gameBoard);
             Pawn pawn = new Pawn(gameBoard[x][y].getPlayerPiece());
@@ -45,30 +50,77 @@ public class Pawn extends Piece{
             list.add(move);
         }
 
-        if(x+forward>-1 && x+forward<gameBoard.length && y-1>-1 && gameBoard[x+forward][y-1]!=null && gameBoard[x][y].getPlayerPiece()!=gameBoard[x+forward][y-1].getPlayerPiece()){
-            Piece[][] possiblePosition = this.copy(gameBoard);
-            Pawn pawn = new Pawn(gameBoard[x][y].getPlayerPiece());
-            pawn.setMoved(true);
-            possiblePosition[x][y] = null;
-            possiblePosition[x+forward][y-1] = pawn;
-            List<String> newNotation = this.generateNewNotation(notation, x, y, x+forward, y-1);
-            Move move = new Move(x, y, x+forward, y-1, possiblePosition, newNotation);
-            list.add(move);
+        if(y-1>-1 && gameBoard[x+forward][y-1]!=null && gameBoard[x][y].getPlayerPiece()!=gameBoard[x+forward][y-1].getPlayerPiece()){
+            if(x+forward==gameBoard.length-1 || x+forward==0){
+                pawnPromotion(gameBoard,x,y,notation,list,forward,-1);
+            }
+            else {
+                Piece[][] possiblePosition = this.copy(gameBoard);
+                Pawn pawn = new Pawn(gameBoard[x][y].getPlayerPiece());
+                pawn.setMoved(true);
+                possiblePosition[x][y] = null;
+                possiblePosition[x + forward][y - 1] = pawn;
+                List<String> newNotation = this.generateNewNotation(notation, x, y, x + forward, y - 1);
+                Move move = new Move(x, y, x + forward, y - 1, possiblePosition, newNotation);
+                list.add(move);
+            }
         }
 
-        if(x+forward>-1 && x+forward<gameBoard.length && y+1<gameBoard[0].length && gameBoard[x+forward][y+1]!=null && gameBoard[x][y].getPlayerPiece()!=gameBoard[x+forward][y+1].getPlayerPiece()){
-            Piece[][] possiblePosition = this.copy(gameBoard);
-            Pawn pawn = new Pawn(gameBoard[x][y].getPlayerPiece());
-            pawn.setMoved(true);
-            possiblePosition[x][y] = null;
-            possiblePosition[x+forward][y+1] = pawn;
-            List<String> newNotation = this.generateNewNotation(notation, x, y, x+forward, y+1);
-            Move move = new Move(x, y, x+forward, y+1, possiblePosition, newNotation);
-            list.add(move);
+        if(y+1<gameBoard[0].length && gameBoard[x+forward][y+1]!=null && gameBoard[x][y].getPlayerPiece()!=gameBoard[x+forward][y+1].getPlayerPiece()){
+            if(x+forward==gameBoard.length-1 || x+forward==0){
+                pawnPromotion(gameBoard,x,y,notation,list,forward,1);
+            }
+            else {
+                Piece[][] possiblePosition = this.copy(gameBoard);
+                Pawn pawn = new Pawn(gameBoard[x][y].getPlayerPiece());
+                pawn.setMoved(true);
+                possiblePosition[x][y] = null;
+                possiblePosition[x + forward][y + 1] = pawn;
+                List<String> newNotation = this.generateNewNotation(notation, x, y, x + forward, y + 1);
+                Move move = new Move(x, y, x + forward, y + 1, possiblePosition, newNotation);
+                list.add(move);
+            }
         }
 
         return list;
     }
 
+    public void pawnPromotion(Piece [][]gameBoard, int x, int y, List<String> notation,List<Move> list,int forward,int yIncrementer){
+        Piece[][] possiblePosition1 = this.copy(gameBoard);
+        Queen queen = new Queen(gameBoard[x][y].getPlayerPiece());
+        queen.setMoved(true);
+        possiblePosition1[x][y] = null;
+        possiblePosition1[x + forward][y+yIncrementer] = queen;
+        List<String> newNotation1 = this.generateNewNotation(notation, x, y, x + forward, y+yIncrementer);
+        Move move1 = new Move(x, y, x + forward, y+yIncrementer, possiblePosition1, newNotation1);
+        list.add(move1);
+
+        Piece[][] possiblePosition2 = this.copy(gameBoard);
+        Rook rook = new Rook(gameBoard[x][y].getPlayerPiece());
+        rook.setMoved(true);
+        possiblePosition2[x][y] = null;
+        possiblePosition2[x + forward][y+yIncrementer] = rook;
+        List<String> newNotation2 = this.generateNewNotation(notation, x, y, x + forward, y+yIncrementer);
+        Move move2 = new Move(x, y, x + forward, y+yIncrementer, possiblePosition2, newNotation2);
+        list.add(move2);
+
+        Piece[][] possiblePosition3 = this.copy(gameBoard);
+        Knight knight = new Knight(gameBoard[x][y].getPlayerPiece());
+        knight.setMoved(true);
+        possiblePosition3[x][y] = null;
+        possiblePosition3[x + forward][y+yIncrementer] = knight;
+        List<String> newNotation3 = this.generateNewNotation(notation, x, y, x + forward, y+yIncrementer);
+        Move move3 = new Move(x, y, x + forward, y+yIncrementer, possiblePosition3, newNotation3);
+        list.add(move3);
+
+        Piece[][] possiblePosition4 = this.copy(gameBoard);
+        Bishop bishop = new Bishop(gameBoard[x][y].getPlayerPiece());
+        bishop.setMoved(true);
+        possiblePosition4[x][y] = null;
+        possiblePosition4[x + forward][y+yIncrementer] = bishop;
+        List<String> newNotation4 = this.generateNewNotation(notation, x, y, x + forward, y+yIncrementer);
+        Move move4 = new Move(x, y, x + forward, y+yIncrementer, possiblePosition4, newNotation4);
+        list.add(move4);
+    }
 
 }
--- ChessBot/src/Test/Driver.java
+++ ChessBot/src/Test/Driver.java
@@ -12,14 +12,13 @@ public class Driver{
 
         List<String> list = new ArrayList<>();
         Bot bot = new Bot();
-        Move move = bot.getNextMove(list,7);
+        Move move = bot.getNextMove(list,1);
 
         bot.printBoard(move.getGameState());
         System.out.println(move.getNextMove());
         double evaluation = move.getEvaluation();
         System.out.println("Evaluation: " + evaluation);
         System.out.println(move.getMoveList());
-        System.out.println(move.getGameState()[2][2].getMoved() + " " + move.getGameState()[2][2].getPlayerPiece() + " " + move.getGameState()[2][2].toString());
     }
 
 }
